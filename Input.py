ebb394d9b7c41bcfb0f4d4f9b3c83d4b9e8ecedf
Bereket Mulugeta <158453418+beckynator@users.noreply.github.com>
2024-02-01 10:01:07 +0000

    My first Program

---
+++ Input.py
@@ -0,0 +1,6 @@
+name = input("what is your name?:")
+print("hi " + name)
+age = input("how old are you?:")
+print("i am  " + age + " too")
+grade = input("which grade are you?:")
+print("good job keep it up " + name)
